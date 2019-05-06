a1af4d8b79332e9ce7fbc5161ebcc9248e6248c7
Ale <ale@192-168-1-102.tpgi.com.au>
2019-05-06 12:07:27 +1000

    Updates

---
+++ precache-manifest.203ca8ac355d30ffe15ae8ad6a0593cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0592229525e5a021d69b44adfbbd79b",
+    "url": "/theAmazingCo/index.html"
+  },
+  {
+    "revision": "2ebf41d6c03d50e1929c",
+    "url": "/theAmazingCo/static/css/main.f5f0b5c0.chunk.css"
+  },
+  {
+    "revision": "c2cec2073f10db4a09ac",
+    "url": "/theAmazingCo/static/js/2.9da904c1.chunk.js"
+  },
+  {
+    "revision": "2ebf41d6c03d50e1929c",
+    "url": "/theAmazingCo/static/js/main.e82b774f.chunk.js"
+  },
+  {
+    "revision": "e4623a9e2b061cad23a7",
+    "url": "/theAmazingCo/static/js/runtime~main.720662bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/theAmazingCo/static/media/logo.5d5d9eef.svg"
+  }
+]);
